72459b1b0801a82f1a12ae8dd21ed9f11ff90e55
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-05-27 22:58:55 +0530

    Add files via upload

---
+++ javascript basics 2/dataTypes.js
@@ -0,0 +1,126 @@
+
+
+//Data Types
+
+// let message = 'hello';
+
+// console.log(message);
+
+// message = 20;
+
+// console.log(message);
+
+
+
+//Number => integer and floating point values 
+
+// let num = 300; //integer
+
+// num = 3.14; floating
+
+//Infinity
+//-Infinity
+//NaN => computational error
+
+// alert(1 / 0)
+// alert(Infinity)
+
+
+// alert('hello' / 20)
+
+// console.log(NaN + 10)
+// console.log(NaN - 10)
+
+// //exception
+
+// console.log(NaN ** 0)
+
+//Big Int
+
+
+//String
+
+// string must be surrounded in quotes
+
+// let name = 'John'; //=> single quotes
+// let name2 = "Peter"; // => double quotes
+// let name3 = `Rahul`; // => backticks are special as they support extend functionality
+
+
+// // console.log(name, name2, name3)
+
+
+// //backticks alows us to embed variables and expression into a string
+
+// console.log(`Hello, ${name}`)
+
+// console.log(`The sum is ${10 + 20}`)
+
+// let name = 'John', city = 'Delhi', country = 'India'
+
+// console.log(`I am  ${name} from ${city} in ${country}`)
+
+
+// //Boolean Type 
+// //true or false
+
+// let nameFieldChecked = true;
+
+// let isLarger = 10 > 1
+
+// console.log(isLarger)
+
+
+
+//Null value
+
+// let age = null;
+
+//nothing, empty or value unknown
+
+
+//undefined
+
+//value not assigned
+
+// let age;
+
+//Objects
+
+//Symbols
+//create unique object keys 
+
+
+// const id = Symbol('id');
+
+// let user = {
+//     name: 'John',
+//     age: 20,
+//     [id]: 2000
+// }
+
+// console.log(user)
+
+
+
+
+console.log(typeof undefined)
+
+console.log(typeof 0)
+
+console.log(typeof 1043578436578346534786538746587346587n)
+
+console.log(typeof true)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript basics 2/script.js
@@ -0,0 +1,50 @@
+
+
+//while loop
+
+// let i = 0;
+
+// while (i > 5) {
+//     console.log(i);
+//     i = i + 1; // i ++ 
+// }
+
+// do {
+//     console.log(i);
+//     i++
+// } while (i > 5)
+
+
+// for (let i = 0; i < 5; i = i + 1) {
+//     console.log(i);
+// }
+
+
+
+// let user = {
+//     name: 'Peter',
+//     age: 30,
+//     isProgrammer: true
+// }
+// //for in loop
+// for (let key in user){
+//     console.log(key)
+//     console.log(user[key])
+// }
+
+
+
+let fruits = ['apples', 'bananas', 'oranges']
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+//for of loop
+
+for (let fruit of fruits) {
+    console.log(fruit)
+}
+
+// for (let key in fruits){
+//     console.log(fruits[key])
+// }
