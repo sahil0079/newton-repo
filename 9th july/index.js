71df7b701c42a7b98cd1f3ad9f4d9368c7e945ef
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-07-09 23:04:18 +0530

    Add files via upload

---
+++ 9th july/index.js
@@ -0,0 +1,25 @@
+
+
+// function printHello() {
+//     console.log('hello')
+// };
+
+// setTimeout(printHello, 2000)
+
+// console.log('Javascript')
+
+
+
+function printHello() {
+    console.log('hello')
+};
+
+
+setTimeout(printHello, 0000)
+
+
+for (let i = 0; i < 100; i++) {
+    console.log(10)
+}
+
+console.log('Javascript')
