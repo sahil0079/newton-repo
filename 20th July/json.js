86cfddf54b3b9119445dc6f3c8c7bfd1e6ca7704
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-07-20 22:54:15 +0530

    Add files via upload

---
+++ 20th July/json.js
@@ -0,0 +1,14 @@
+
+
+// const jsonData = '{"name":"John", "age":30, "city":"New York"}'
+
+
+// const jsonObject = JSON.parse(jsonData);
+
+// console.log(jsonObject)
+
+
+// const jsonObject = { name: "John", age: 30 };
+// const jsonData = JSON.stringify(jsonObject);
+// console.log(jsonData)
+
